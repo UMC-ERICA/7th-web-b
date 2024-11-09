49bc9f596c08b604137e5a02abed9c540f5431f9
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2024-11-09 22:30:10 +0900

    Revert "[FEAT] week6 search page"

--- 움/week6/MoviePage4/src/components/MovieCardListSkeleton.jsx
+++
@@ -1,9 +0,0 @@
-import MovieCardSkeleton from "./MovieCardSkeleton"
-
-const MovieCardListSkeleton = ({number}) => {
-    return (
-        new Array(number).fill(0).map((_, ) => <MovieCardSkeleton />)
-    );
-};
-
-export default MovieCardListSkeleton;
